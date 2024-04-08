ca7694c7454bcb2d8a7b3cd9551abe62eef7579a
branberry <brandonly@lostcoding.com>
2024-04-08 08:48:00 -0500

    update globals

--- gamemode/sh_globals.lua
+++ gamemode/sh_globals.lua
@@ -1,3 +1,8 @@
-include("classes/mage.lua")
-CLASSES = {MAGE}
-CC_CHANGE_CLASS = "cc_change_class"
+CC_CHANGE_CLASS = 'cc_change_class'
+CLASSES = {}
+print(#file.Find(GM.FolderName .. '/gamemode/classes/*.lua', 'LUA'))
+for _, classFile in pairs(file.Find(GM.FolderName .. '/gamemode/*.lua', 'LUA')) do
+  print(classFile)
+  -- local class = require('classes/' .. classFile)
+  -- print(class)
+end
